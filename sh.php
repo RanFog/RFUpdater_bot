4812c879990956d1244a61f8930caec74d629edb
C.C.Tishchenko <constish@gmail.com>
2019-04-05 00:02:12 +0300

    Update_updater

--- sh.php
+++ sh.php
@@ -10,4 +10,15 @@ function update_test()
     return shell_exec ("sh sh/FRTEST.sh");
     //return "Обновлено";
     }
+function update_update()
+    {
+    //echo shell_exec("sh -S /var/www/html/telebots/FRUPDATER/sh/FRTEST.sh");
+    //return shell_exec ("ls -la");
+    //return shell_exec ("ping -c 4 ya.ru");
+    //return shell_exec ("whoami"); //www-data
+    //return shell_exec ("groups"); //www-data
+    //return shell_exec ("sudo mkdir 121212");
+    return shell_exec ("cd /var/www/html/telebots/FRUPDATER/").shell_exec ("git pull");
+    //return "Обновлено";
+    }
 ?>
