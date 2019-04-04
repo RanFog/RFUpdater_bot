2081243354f60d33de45010a8ab6f6afcdd3331f
C.C.Tishchenko <constish@gmail.com>
2019-04-04 22:06:50 +0300

    exec_15

--- sh.php
+++ sh.php
@@ -3,7 +3,8 @@ function update_test()
     {
     //echo shell_exec("sh -S /var/www/html/telebots/FRUPDATER/sh/FRTEST.sh");
     //return shell_exec ("ls -la");
-    return shell_exec ("ping -c 4 ya.ru");
+    //return shell_exec ("ping -c 4 ya.ru");
+    return shell_exec ("mkdir 123456677");
     //return "Обновлено";
     }
 ?>
