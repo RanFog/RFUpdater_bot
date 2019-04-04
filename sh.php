441cbe3202840256e8fdcb9757bf7c3ac266b7a6
C.C.Tishchenko <constish@gmail.com>
2019-04-04 22:17:44 +0300

    exec_18

--- sh.php
+++ sh.php
@@ -4,7 +4,10 @@ function update_test()
     //echo shell_exec("sh -S /var/www/html/telebots/FRUPDATER/sh/FRTEST.sh");
     //return shell_exec ("ls -la");
     //return shell_exec ("ping -c 4 ya.ru");
-    return shell_exec ("whoami");
+    //return shell_exec ("whoami"); //www-data
+    //return shell_exec ("groups"); //www-data
+    shell_exec ("mkdir 121212");
+    return shell_exec ("ls -la");
     //return "Обновлено";
     }
 ?>
