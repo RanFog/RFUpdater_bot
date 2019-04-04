5d11fbeb946814b00979feaef9193e8d2ba8adde
C.C.Tishchenko <constish@gmail.com>
2019-04-04 21:40:14 +0300

    exec_11

--- sh.php
+++ sh.php
@@ -2,7 +2,7 @@
 function update_test()
     {
     //echo shell_exec("sh -S /var/www/html/telebots/FRUPDATER/sh/FRTEST.sh");
-    return shell_exec ("mkdir /var/www/html/telebots/FRUPDATER/testtesttest");
-    return "Обновлено";
+    return shell_exec ("ls -la");
+    //return "Обновлено";
     }
 ?>
