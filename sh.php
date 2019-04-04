696a1cfaa17206b2f23b41ec93b9eb2a8757b4f8
C.C.Tishchenko <constish@gmail.com>
2019-04-04 22:38:49 +0300

    exec_20

--- sh.php
+++ sh.php
@@ -6,7 +6,7 @@ function update_test()
     //return shell_exec ("ping -c 4 ya.ru");
     //return shell_exec ("whoami"); //www-data
     //return shell_exec ("groups"); //www-data
-    return shell_exec ("mkdir 121212");
+    return shell_exec ("sudo mkdir 121212");
     return shell_exec ("ls -la");
     //return "Обновлено";
     }
